2c935570ff7ffad365e379f03982b2cc519c4f0e
Omar <omarmohmed7659944@gmail.com>
2024-10-05 00:14:27 +0300

    pushing to  remote

--- game/Serializer.py
+++ game/Serializer.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
-
-from . models import *
-
+from .models import Users
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = '__all__'
-        read_only_fields = ['user']
+        fields = '__all__'  # Use all fields from the model
+        read_only_fields = ['user']  # Mark 'user' as read-only
+
+    def to_internal_value(self, data):
+        # Filter out any extra fields that are not part of the model
+        allowed_fields = set(self.fields)  # Get all fields defined in the serializer
+        filtered_data = {key: value for key, value in data.items() if key in allowed_fields}
+
+        return super(UserSerializer, self).to_internal_value(filtered_data)
